8ae50ec57608b41b1bebf0842bd46086f29e8a3b
smrockv <smrockv@gmail.com>
2025-05-08 02:19:59 +0000

    base urlを変更

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? '/recepit/' : './',
+  //base: process.env.GITHUB_PAGES ? '/recepit/' : './',
+  base: '/recepit/',
   plugins: [react()],
 })
