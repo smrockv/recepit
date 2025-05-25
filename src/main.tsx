b32bc97476a4d4ff23373c628330a48c42e23f94
smrockv <smrockv@gmail.com>
2025-05-25 05:44:11 +0000

    darkテーマを導入

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createTheme, ThemeProvider } from '@mui/material';
+//import './index.css'
+import CssBaseline from '@mui/material/CssBaseline';
 import App from './App.tsx'
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </StrictMode>
 )
